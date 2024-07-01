2635cca37f053bcf64cc3ae33b92f5511c6bc62c
Trendevice <trendevice@Mac-mini-di-Trendevice.local>
2024-07-01 11:08:36 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import { lusitana } from './ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Page() {
+  console.log('apap')
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -17,6 +20,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <p className={`${lusitana.className}`}>Lusitana text</p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -26,6 +30,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+          src='/hero-mobile.png'
+          width={560}
+          height={620}
+          className='block md:hidden '
+          alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
